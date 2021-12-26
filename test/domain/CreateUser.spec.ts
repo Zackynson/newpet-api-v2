da654cf0df1c8b7f80354efdb1652e2fb62788f7
zackynson <crys.chb@hotmail.com>
2021-12-26 16:47:56 -0300

    Ensure UsersRepository won't register same email twice

--- test/domain/CreateUser.spec.ts
+++ test/domain/CreateUser.spec.ts
@@ -14,16 +14,31 @@ class UsersRepositoryMock implements UsersRepository{
 
 
   async insert({name, email, password}: User): Promise<void> {
+      if(this.users.some(user => user.email === email)) return 
       this.users.push({name, email, password})
   }
 }
 
+const makeSut = () => {
+  const sut = new UsersRepositoryMock()
+  const user = { name: 'any_name', email: 'any_email', password:'any_password'}
 
+  return { sut, user }
+}
+ 
 describe('CreateUser', () => {
   test('should create a user', async () => {
-    const sut = new UsersRepositoryMock()
-    const user = { name: 'any_name', email: 'any_email', password:'any_password'}
+    const { sut, user } = makeSut()
 
+    await sut.insert(user);
+
+    expect(sut.users).toEqual([user])
+  })
+
+  test('should not create two users with same email', async () => {
+    const { sut, user } = makeSut()
+
+    await sut.insert(user);
     await sut.insert(user);
 
     expect(sut.users).toEqual([user])
