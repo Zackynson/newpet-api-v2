e9b96851b411c61e318ab93fc5810f6b62c1814f
zackynson <crys.chb@hotmail.com>
2021-12-27 17:08:24 -0300

    Ensure CreateUserUseCase throws if UserRepository throws when creating an user

--- test/domain/useCases/Users/CreateUserUseCase.spec.ts
+++ test/domain/useCases/Users/CreateUserUseCase.spec.ts
@@ -18,4 +18,28 @@ describe('CreateUserUseCase', () => {
 
     expect(usersRepository.users[0]).toEqual(user)
   })
+
+  test('Should not create a user when email already registered', async() => {
+    const usersRepository = new UsersRepositoryMock()
+    const sut = new CreateUserUseCase(usersRepository)
+
+    const user: User = {
+      email: 'same@email.com',
+      name: 'any_name',
+      avatar_url: 'any_url',
+      password:'any_password'
+    }
+
+    const user2: User = {
+      email: 'same@email.com',
+      name: 'another_name',
+      avatar_url: 'another_url',
+      password:'another_password'
+    }
+
+    await sut.execute(user)
+
+    await expect(sut.execute(user2)).rejects.toThrow()
+  })
+
 })
