a75d2f331b608441d750402a528f554f2f7c322c
zackynson <crys.chb@hotmail.com>
2021-12-30 19:38:51 -0300

    Ensure UpdatePetUseCase throws if an  pet is not on usersAccount

--- test/data/Pets/update-pet-usecase.spec.ts
+++ test/data/Pets/update-pet-usecase.spec.ts
@@ -39,4 +39,22 @@ describe('UpdatePetUseCase', () => {
 
     await expect(promise).rejects.toThrow('User not found');
   });
+
+  test('Should throw an error if pet is not from expecified user', async () => {
+    const pet:UpdatePetParams = {
+      name: 'any_name',
+      age: 1,
+      category: 'cat',
+    };
+
+    const petsRepository = new MemoryPetsRepository();
+    const usersRepository = new MemoryUsersRepository();
+    const updatePetUseCase = new UpdatePetUseCase(petsRepository, usersRepository);
+
+    await usersRepository.mockUsersList();
+
+    const promise = updatePetUseCase.execute({ petId: 'any_id', ownerId: '1', data: pet });
+
+    await expect(promise).rejects.toThrow('Pet not found on users account');
+  });
 });
