aae6e42b5dbad436fabeb46ffd786d7eea6ac53e
Zackynson <crystopher.carvalho@universo.univates.br>
2022-01-04 14:28:04 -0300

    test: ensure SignUpController returns 500 if EmailValidator throws

--- test/presentation/controllers/SignUpController.spec.ts
+++ test/presentation/controllers/SignUpController.spec.ts
@@ -1,45 +1,58 @@
 /* eslint-disable max-classes-per-file */
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/Http';
-import { Controller } from '@/presentation/protocols/Controller';
-import { EmailValidator } from '../protocols/EmailValidator';
+import { Controller, EmailValidator } from '@/presentation/protocols';
+import { InvalidParamError, MissingParamError, ServerError } from '@/presentation/errors';
 
 export class SignUpControllerMock implements Controller {
   constructor(private readonly emailValidator: EmailValidator) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { name, email } = httpRequest.body || {};
+    try {
+      const { name, email } = httpRequest.body || {};
 
-    if (!name) {
+      if (!name) {
+        return {
+          data: new MissingParamError('name'),
+          statusCode: 400,
+        };
+      }
+
+      if (!email) {
+        return {
+          data: new MissingParamError('email'),
+          statusCode: 400,
+        };
+      }
+
+      const emailIsValid = await this.emailValidator.validate(email);
+      if (!emailIsValid) {
+        return {
+          data: new InvalidParamError('email'),
+          statusCode: 400,
+        };
+      }
+
       return {
-        data: new Error('Missing Param: name'),
-        statusCode: 400,
+        statusCode: 200,
+      };
+    } catch (error) {
+      return {
+        data: new ServerError(),
+        statusCode: 500,
       };
     }
-
-    if (!email) {
-      return {
-        data: new Error('Missing Param: email'),
-        statusCode: 400,
-      };
-    }
-
-    const emailIsValid = await this.emailValidator.validate(email);
-    if (!emailIsValid) {
-      return {
-        data: new Error('Invalid Param: email'),
-        statusCode: 400,
-      };
-    }
-
-    return {
-      statusCode: 200,
-    };
   }
 }
 
 class EmailValidatorStub implements EmailValidator {
   validate(email: string): boolean {
     return true;
+  }
+}
+
+class EmailValidatorWithError implements EmailValidator {
+  validate(email: string): boolean {
+    throw new Error();
   }
 }
 
@@ -66,7 +79,7 @@ describe('SignUpController', () => {
     const response = await sut.handle(request);
 
     expect(response.statusCode).toBe(400);
-    expect(response.data).toEqual(new Error('Missing Param: name'));
+    expect(response.data).toEqual(new MissingParamError('name'));
   });
 
   test('Should returns 400 if no email is provided', async () => {
@@ -84,7 +97,7 @@ describe('SignUpController', () => {
     const response = await sut.handle(request);
 
     expect(response.statusCode).toBe(400);
-    expect(response.data).toEqual(new Error('Missing Param: email'));
+    expect(response.data).toEqual(new MissingParamError('email'));
   });
 
   test('Should returns 400 if an invalid mail is provided', async () => {
@@ -104,6 +117,26 @@ describe('SignUpController', () => {
     const response = await sut.handle(request);
 
     expect(response.statusCode).toBe(400);
-    expect(response.data).toEqual(new Error('Invalid Param: email'));
+    expect(response.data).toEqual(new InvalidParamError('email'));
+  });
+
+  test('Should returns 500 if emailValidator throws', async () => {
+    const emailValidatorWithError = new EmailValidatorWithError();
+    const sut = new SignUpControllerMock(emailValidatorWithError);
+
+    const request = {
+      body: {
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password',
+        confirmPassword: 'any_password',
+        avatarUrl: 'any_password',
+      },
+    };
+
+    const response = await sut.handle(request);
+
+    expect(response.statusCode).toBe(500);
+    expect(response.data).toEqual(new ServerError());
   });
 });
