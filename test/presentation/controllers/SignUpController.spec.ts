7b92ecb23cbe8c6e5407a80adb8760220481e147
Zackynson <crystopher.carvalho@universo.univates.br>
2022-01-04 13:42:37 -0300

    test: ensure SignUpController returns 400 if no name is provided

---
+++ test/presentation/controllers/SignUpController.spec.ts
@@ -0,0 +1,37 @@
+import { HttpRequest, HttpResponse } from '@/presentation/protocols/Http';
+import { Controller } from '@/presentation/protocols/Controller';
+
+export class SignUpControllerStub implements Controller {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    if (!httpRequest.body.name) {
+      return {
+        data: new Error('Missing Param: name'),
+        statusCode: 400,
+      };
+    }
+
+    return {
+      statusCode: 200,
+    };
+  }
+}
+
+describe('SignUpController', () => {
+  test('Should returns 400 if no name is informed', async () => {
+    const sut = new SignUpControllerStub();
+
+    const request = {
+      body: {
+        email: 'any_email',
+        password: 'any_password',
+        confirmPassword: 'any_password',
+        avatarUrl: 'any_password',
+      },
+    };
+
+    const response = await sut.handle(request);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.data).toEqual(new Error('Missing Param: name'));
+  });
+});
