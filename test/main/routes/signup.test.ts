c6ae431e21f1f6f0036d3734ee5b105f838eac8e
zackynson <crys.chb@hotmail.com>
2022-01-09 18:56:26 -0300

    test: connect to mongodb before tests

--- test/main/routes/signup.test.ts
+++ test/main/routes/signup.test.ts
@@ -1,7 +1,18 @@
 import request from 'supertest';
 import app from '@/main/config/app';
+import { MongoHelper } from '@/infra/db/mongodb/helpers/MongoHelper';
+
+const mongoHelper = new MongoHelper();
 
 describe('SignUp Route', () => {
+  beforeAll(async () => { await mongoHelper.connect(process.env.MONGO_URL); });
+  afterAll(async () => { await mongoHelper.disconnect(); });
+
+  beforeEach(async () => {
+    const collection = await mongoHelper.getCollection('users');
+    await collection.deleteMany({});
+  });
+
   test('Should return 200', async () => {
     await request(app).post('/signup').send()
       .expect(200);
