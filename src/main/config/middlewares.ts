446e3dbfe612541d57034a621b6fe8b85c7085b2
zackynson <crys.chb@hotmail.com>
2022-01-09 18:14:54 -0300

    refactor: move imports to single file

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,5 @@
 import { Express } from 'express';
-import { bodyParser } from '@/main/middlewares/bodyParser';
-import { cors } from '@/main/middlewares/cors';
-import { contentType } from '@/main/middlewares/contentType';
+import { bodyParser, cors, contentType } from '@/main/middlewares';
 
 export default (app: Express): void => {
   app.use(cors);
