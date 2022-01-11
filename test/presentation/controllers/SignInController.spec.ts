772fb2457e4c0670ec6799d448ef0bec6aa78ed8
Zackynson <crystopher.carvalho@universo.univates.br>
2022-01-11 18:17:23 -0300

    test: ensure SignInController returns 400 if no password is provided

--- test/presentation/controllers/SignInController.spec.ts
+++ test/presentation/controllers/SignInController.spec.ts
@@ -18,4 +18,19 @@ describe('SignInController', () => {
     expect(response.statusCode).toBe(400);
     expect(response.data).toEqual(new MissingParamError('email'));
   });
+
+  test('Should returns 400 if no password is provided', async () => {
+    const sut = makeSut();
+
+    const request = {
+      body: {
+        email: 'any_email',
+      },
+    };
+
+    const response = await sut.handle(request);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.data).toEqual(new MissingParamError('password'));
+  });
 });
