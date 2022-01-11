f9739b0520a7577b16f370acbbcfa196fddc6247
Zackynson <crystopher.carvalho@universo.univates.br>
2022-01-11 18:14:59 -0300

    test: ensure SignInController returns 400 if no email is provided

---
+++ test/presentation/controllers/SignInController.spec.ts
@@ -0,0 +1,21 @@
+import { MissingParamError } from '@/presentation/errors';
+import { SignInController } from '@/presentation/controllers';
+
+const makeSut = (): SignInController => new SignInController();
+
+describe('SignInController', () => {
+  test('Should returns 400 if no email is provided', async () => {
+    const sut = makeSut();
+
+    const request = {
+      body: {
+        password: 'any_password',
+      },
+    };
+
+    const response = await sut.handle(request);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.data).toEqual(new MissingParamError('email'));
+  });
+});
