49c7649b24fbdd87bcf45930669b97a910097ed6
Zackynson <crystopher.carvalho@universo.univates.br>
2022-01-11 18:17:14 -0300

    feat: ensure SignInController returns 400 if no password is provided

--- src/presentation/controllers/SignIn.ts
+++ src/presentation/controllers/SignIn.ts
@@ -1,12 +1,14 @@
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols';
-import { badRequest } from '@/presentation/helpers';
+import { badRequest, ok } from '@/presentation/helpers';
 import { MissingParamError } from '@/presentation/errors';
 
 export class SignInController implements Controller {
   async handle(httpRequest:HttpRequest):Promise<HttpResponse> {
-    const { email } = httpRequest.body;
+    const { email, password } = httpRequest.body;
 
     if (!email) return badRequest(new MissingParamError('email'));
-    return null;
+    if (!password) return badRequest(new MissingParamError('password'));
+
+    return ok();
   }
 }
