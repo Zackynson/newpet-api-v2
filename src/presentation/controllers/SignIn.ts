4291bf99a72ceb33f36c53e4830ec99c2579e794
Zackynson <crystopher.carvalho@universo.univates.br>
2022-01-11 18:28:37 -0300

    feat: ensure SignInController returns 400 if an invalid email is provided

--- src/presentation/controllers/SignIn.ts
+++ src/presentation/controllers/SignIn.ts
@@ -1,13 +1,23 @@
-import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols';
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+  EmailValidator,
+} from '@/presentation/protocols';
 import { badRequest, ok } from '@/presentation/helpers';
-import { MissingParamError } from '@/presentation/errors';
+import { MissingParamError, InvalidParamError } from '@/presentation/errors';
 
 export class SignInController implements Controller {
+  constructor(private readonly emailValidator: EmailValidator) {}
+
   async handle(httpRequest:HttpRequest):Promise<HttpResponse> {
     const { email, password } = httpRequest.body;
 
     if (!email) return badRequest(new MissingParamError('email'));
     if (!password) return badRequest(new MissingParamError('password'));
+
+    const isEmailValid = this.emailValidator.validate(email);
+    if (!isEmailValid) return badRequest(new InvalidParamError('email'));
 
     return ok();
   }
