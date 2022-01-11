df2d30a43d8b5bd16addcca59a849b7a410e3f0a
Zackynson <crystopher.carvalho@universo.univates.br>
2022-01-11 19:53:45 -0300

    feat: ensure MongoHelper reconnect on getCollection if mongo is down

--- src/infra/db/mongodb/helpers/MongoHelper.ts
+++ src/infra/db/mongodb/helpers/MongoHelper.ts
@@ -1,16 +1,24 @@
 import { MongoClient, Collection } from 'mongodb';
 
 export const MongoHelper = {
+  client: null as MongoClient,
+  url: null as string,
 
   async connect(url: string):Promise<void> {
     this.client = await MongoClient.connect(url);
+    this.url = url;
   },
 
   async disconnect():Promise<void> {
     await this.client.close();
+    this.client = null;
   },
 
   async getCollection(name:string):Promise<Collection> {
+    if (!this.client?.db) {
+      await this.connect(this.url);
+    }
+
     return this.client.db().collection(name);
   },
 };
